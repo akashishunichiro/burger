34d2d4e35eda598e81fcc1fc8460d14395c608f6
akashishunichiro <168219474+akashishunichiro@users.noreply.github.com>
2024-05-01 13:58:08 +0500

    fastfood

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from config import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('burger.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('payment/', include('payment.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
